95b388d975eb7fa6fff089bcee9a97d22e69b7b0
ManuAndrei <brreaker@yahoo.com>
2015-10-15 13:38:45 +0200

    Added Database

---
+++ CentroDeportivo/Control.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CentroDeportivo
+{
+    public partial class Control : Form
+    {
+        public Database db = new Database();
+        public Control()
+        {
+            InitializeComponent();
+        }
+
+        public bool SignUp(Socio soc) {
+            return db.Socios.Add(soc);
+        }
+
+        public bool Remove(Socio soc)
+        {
+            return db.Socios.Remove(soc);
+        }
+
+        public bool Reserva(Alquiler al, Instalacion inst) {
+            bool InstalacionDisponible = true;
+            foreach (Alquiler a in db.Alquileres)
+            {
+                if ((a.FechaAlquiler > al.FechaAlquiler) && (a.FechaVencimiento <= al.FechaVencimiento))
+                    InstalacionDisponible = false;
+            }
+            if ((al.InstalacionReservada == null) && InstalacionDisponible)
+            {
+                al.InstalacionReservada = inst;
+                inst.Reservar(al);
+                return db.Alquileres.Add(al);
+            }
+            return false;
+            
+        }
+
+        public HashSet<Alquiler> ConsultarAlquileres() {
+            return db.Alquileres;
+        }
+
+        public HashSet<Socio> ConsultarSocios() {
+            return db.Socios;
+        }
+
+        public HashSet<Instalacion> ConsultarInstalaciones() {
+            return db.Instalaciones;
+        }
+
+        
+
+    }
+}
---
+++ CentroDeportivo/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentroDeportivo
+{
+    public class Database
+    {
+        public Database()
+        {
+            Socios = new HashSet<Socio>();
+            Alquileres = new HashSet<Alquiler>();
+            Actividades = new HashSet<Actividad>();
+            Instalaciones = new HashSet<Instalacion>();
+        }
+        public HashSet<Socio> Socios { get; set; }
+        public HashSet<Alquiler> Alquileres { get; set; }
+        public HashSet<Actividad> Actividades { get; set; }
+        public HashSet<Instalacion> Instalaciones { get; set; }
+
+    }
+}
